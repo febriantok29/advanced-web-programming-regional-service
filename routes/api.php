a99f00e23e5bbd74f0b8f35a83a8f21e7a808c11
febriantok29 <febriantokabisatulloh@rocketmail.com>
2024-11-19 06:41:50 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,25 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
 use App\Http\Controllers\RegionalController;
 use App\Http\Controllers\CustomerController;
 
@@ -16,12 +35,7 @@ Route::prefix('api')->group(function () {
     Route::post('customer', [CustomerController::class, 'store']);
 });
 
-Route::middleware('api')
-    ->namespace($this->namespace)
-    ->group(base_path('routes/api.php'));
-
 
 Route::get('test', function () {
     return response()->json(['message' => 'API works']);
 });
-
